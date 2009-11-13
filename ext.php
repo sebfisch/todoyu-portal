ab85e9848be0aafcb0f4c4d258300e9fe54f9aff
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-13 11:22:10 +0000

    * cleaned CSS files to comply with code style conventions * added 'show in project' icon to portal context menu

--- ext.php
+++ ext.php
@@ -7,7 +7,7 @@
 *
 *  This script is part of the todoyu project.
 *  The todoyu project is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License, version 2, 
+*  it under the terms of the GNU General Public License, version 2,
 *  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
 *  the Free Software Foundation;
 *
