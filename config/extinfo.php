b23ae8907b2e4d83ed414856a7077df099ac699c
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-18 09:34:36 +0000

    * Bugfixes for loading extensions

--- config/extinfo.php
+++ config/extinfo.php
@@ -7,7 +7,7 @@
 *
 *  This script is part of the todoyu project.
 *  The todoyu project is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License, version 2, 
+*  it under the terms of the GNU General Public License, version 2,
 *  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
 *  the Free Software Foundation;
 *
@@ -28,12 +28,10 @@ $CONFIG['EXT']['portal']['info'] = array(
 	'title'				=> 'Portal',
 	'description' 		=> 'Portal area (Quick overview of various aspects of todoyu)',
 	'author' 			=> array(
-		'name'		=> 'Kay Stenschke',
-		'email'		=> 'kstenschke@snowflake.ch',
+		'name'		=> 'Todoyu Core Team',
+		'email'		=> 'team@todoyu.com',
 		'company'	=> 'Snowflake Productions, Zürich'
 	),
-	'dependencies' 		=> 'project,search,user',
-	'conflicts'			=> '',
 	'state' 			=> 'beta',
 	'version' => '0.1.0',
 	'constraints' => array(
@@ -42,10 +40,7 @@ $CONFIG['EXT']['portal']['info'] = array(
 		),
 		'conflicts' => array(
 
-		),
-		'suggests' => array(
-
-		),
+		)
 	)
 );
 
