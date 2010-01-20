81c360b5d8e47c5ac7f1e88881acf080ba481394
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-01-20 11:00:07 +0000

    * Added TodoyuCurrency * Finishing Locale * Added locale to installer template

--- controller/TodoyuPortalExtActionController.class.php
+++ controller/TodoyuPortalExtActionController.class.php
@@ -67,9 +67,7 @@ class TodoyuPortalExtActionController extends TodoyuActionController {
 
 		TodoyuPortalManager::addTabAssetsToPage();
 
-//		TodoyuDebug::printHtml(setlocale(LC_ALL, 0));
-//		TodoyuDebug::printHtml(Todoyu::getLocale());
-//
+		TodoyuDebug::printHtml(TodoyuLocaleManager::getLocaleOptions());
 
 			// Context menu
 		TodoyuPage::addJsOnloadedFunction('Todoyu.Ext.project.ContextMenuTask.attach.bind(Todoyu.Ext.project.ContextMenuTask)');
