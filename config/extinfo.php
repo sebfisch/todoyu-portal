8556687d58411f183b632442feb1be3778b7efb9
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-03-29 15:22:53 +0000

    * Changed version number of all extensions to 0.3.0 (ready to branch)

--- config/extinfo.php
+++ config/extinfo.php
@@ -32,10 +32,10 @@ Todoyu::$CONFIG['EXT']['portal']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state' 		=> 'beta',
-	'version' 		=> '0.2.0',
+	'version' 		=> '0.3.0',
 	'constraints' => array(
 		'depends' => array(
-			'project' => '0.2.0'
+			'project' => '0.3.0'
 		),
 		'conflicts' => array(
 
