3a41ed165948a21b26f251b2b598464114b8808a
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-03-14 09:57:31 +0000

    * Version cleanup

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,13 +31,12 @@ Todoyu::$CONFIG['EXT']['portal']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version'		=> '1.2.0a',
+	'version'		=> '1.3.0a',
 	'constraints'	=> array(
 		'depends'	=> array(
 			'project'	=> '1.2.0',
 			'search'	=> '1.2.0'
-		),
-		'conflicts' => array()
+		)
 	),
 	'urlDocumentation'	=> 'http://doc.todoyu.com/?portal'
 );
